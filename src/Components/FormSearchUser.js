b020a39897f2839d8303e345ee7cc0e3e8f480f0
Siemon <alexandre.lambert@edu.ece.fr>
2019-10-25 17:25:32 +0200

    transfert blindage ok

--- src/Components/FormSearchUser.js
+++ src/Components/FormSearchUser.js
@@ -1,84 +1,90 @@
 import React, { Component } from "react";
 import {
-  Button,
-  FormControl,
-  ListGroup,
-  FormGroup,
-  FormLabel
+    Button,
+    FormControl,
+    ListGroup,
+    FormGroup,
+    FormLabel
 } from "react-bootstrap";
+import { returnUser, findBalance } from "../Utils/utils";
 class FormSearchUser extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userSearch: "",
-      userResult: [],
-      userSelect: ""
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.searchUser = this.searchUser.bind(this);
-    this.handleSelect = this.handleSelect.bind(this);
-  }
-  handleChange(event) {
-    this.setState(
-      {
-        [event.target.name]: event.target.value
-      },
-      this.searchUser
-    );
-  }
-  searchUser() {
-    let users = JSON.parse(localStorage.getItem("Users"));
-    let userLike = [];
-    users.forEach(user => {
-      if (
-        user.email.includes(this.state.userSearch) &&
-        this.state.userSearch != ""
-      ) {
-        
-        userLike.push(user.email);
-      }
-    });
-    this.setState({
-      userResult: userLike
-    });
-    this.props.onChangeUserSelect("");
-    this.setState({
-        userSelect : ""
-    })
-  }
-  handleSelect(eventKey) {
-    this.setState(
-      {
-        userSelect: eventKey
-      },
-      this.props.onChangeUserSelect(eventKey)
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            userSearch: "",
+            userResult: [],
+            userSelect: ""
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.searchUser = this.searchUser.bind(this);
+        this.handleSelect = this.handleSelect.bind(this);
+    }
+    handleChange(event) {
+        this.setState(
+            {
+                [event.target.name]: event.target.value
+            },
+            this.searchUser
+        );
+    }
+    searchUser() {
+        let users = JSON.parse(localStorage.getItem("Users"));
+        let userLike = [];
+        users.forEach(user => {
+            if (
+                user.email.includes(this.state.userSearch) &&
+                this.state.userSearch != ""
+            ) {
+                if (user.email != returnUser().email) {
+                    userLike.push(user.email);
+                }
+            }
+        });
+        this.setState({
+            userResult: userLike
+        });
+        this.props.onChangeUserSelect("");
+        this.setState({
+            userSelect: ""
+        });
+    }
+    handleSelect(eventKey) {
+        this.setState(
+            {
+                userSelect: eventKey
+            },
+            this.props.onChangeUserSelect(eventKey)
+        );
+    }
 
-  render() {
-    return (
-      <div>
-        <FormGroup>
-          <FormLabel>Search user</FormLabel>
-          <FormControl
-            type="text"
-            placeholder="User"
-            className="mr-sm-2"
-            name="userSearch"
-            onChange={this.handleChange}
-            value={this.state.userSearch}
-          />
-        </FormGroup>
-        <ListGroup onSelect={this.handleSelect}>
-          Result of search
-          {this.state.userResult.map(result => (
-            <ListGroup.Item active={false} key={result} eventKey={result}>
-              {result}
-            </ListGroup.Item>
-          ))}
-        </ListGroup>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <FormGroup>
+                    <FormLabel>Search user</FormLabel>
+                    <FormControl
+                        type="text"
+                        placeholder="User"
+                        className="mr-sm-2"
+                        name="userSearch"
+                        onChange={this.handleChange}
+                        value={this.state.userSearch}
+                    />
+                </FormGroup>
+                <ListGroup onSelect={this.handleSelect}>
+                    Result of search
+                    {this.state.userResult.map(result => (
+                        <ListGroup.Item
+                            active={false}
+                            key={result}
+                            eventKey={result}
+                        >
+                            {result}
+                        </ListGroup.Item>
+                    ))}
+                </ListGroup>
+            </div>
+        );
+    }
 }
 export default FormSearchUser;
--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -1,83 +1,110 @@
 import React, { Component } from "react";
-import { Form, Button, Container } from "react-bootstrap";
+import { Form, Button, Container, Alert } from "react-bootstrap";
 import FormSearchUser from "./FormSearchUser";
-import { checkId, findUserId } from "../Utils/utils";
+import { checkId, findUserId, findBalance } from "../Utils/utils";
 class Transaction extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      amountTransfert: 0,
-      transfert: {
-        id: 0,
-        debited_wallet_id: 0,
-        credited_wallet_id: 0,
-        amout: 0
-      }
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  }
-  handleSubmit() {
-    let userIdIn = findUserId(this.props.userSelect);
-    let userOut = JSON.parse(localStorage.getItem("connectUser"));
-    let wallets = JSON.parse(localStorage.getItem("Wallets"));
-    let transferts = localStorage.getItem("Transfers")
-      ? JSON.parse(localStorage.getItem("Transfers"))
-      : [];
-    let transfert = this.state.transfert;
-
-    if (this.state.amountTransfert >= 1) {
-      transfert.amout = this.state.amountTransfert * 100;
-      transfert.id = checkId(transferts);
-      wallets.forEach(wallet => {
-        if (wallet.userId == userIdIn) {
-          wallet.balance += this.state.amountTransfert * 100;
-          transfert.credited_wallet_id = wallet.id;
-        } else if (wallet.userId == userOut.id) {
-          wallet.balance -= this.state.amountTransfert * 100;
-          transfert.debited_wallet_id = wallet.id;
-        }
-      });
-      this.setState({
-        transfert: transfert
-      });
-      transferts.push(this.state.transfert);
-      localStorage.setItem("Wallets", JSON.stringify(wallets));
-      localStorage.setItem("Transfers", JSON.stringify(transferts));
-
-      this.props.onChangeTransaction();
-    } else {
-      alert("minimum transfert is 1 $");
+    constructor(props) {
+        super(props);
+        this.state = {
+            amountTransfert: 0,
+            transfert: {
+                id: 0,
+                debited_wallet_id: 0,
+                credited_wallet_id: 0,
+                amout: 0
+            },
+            displayAlert: false,
+            alertMessage: ""
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-  }
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+    handleSubmit() {
+        let userIdIn = findUserId(this.props.userSelect);
+        let userOut = JSON.parse(localStorage.getItem("connectUser"));
+        let wallets = JSON.parse(localStorage.getItem("Wallets"));
+        let transferts = localStorage.getItem("Transfers")
+            ? JSON.parse(localStorage.getItem("Transfers"))
+            : [];
+        let transfert = this.state.transfert;
 
-  render() {
-    return (
-      <div>
-        <Container>
-          <Form>
-            <Form.Group>
-              <Form.Label>
-                Transfers {this.state.amountTransfert} to{" "}
-                {this.props.userSelect}{" "}
-              </Form.Label>
-              <Form.Control
-                type="number"
-                name="amountTransfert"
-                value={this.state.amountTransfert}
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-            <Button onClick={this.handleSubmit}>Transfers</Button>
-          </Form>
-        </Container>
-      </div>
-    );
-  }
+        if (this.state.amountTransfert >= 1) {
+            transfert.amout = this.state.amountTransfert * 100;
+            transfert.id = checkId(transferts);
+            wallets.forEach(wallet => {
+                if (wallet.userId == userIdIn) {
+                    wallet.balance += this.state.amountTransfert * 100;
+                    transfert.credited_wallet_id = wallet.id;
+                } else if (wallet.userId == userOut.id) {
+                    wallet.balance -= this.state.amountTransfert * 100;
+                    transfert.debited_wallet_id = wallet.id;
+                }
+            });
+            this.setState({
+                transfert: transfert
+            });
+            transferts.push(this.state.transfert);
+            localStorage.setItem("Wallets", JSON.stringify(wallets));
+            localStorage.setItem("Transfers", JSON.stringify(transferts));
+
+            this.props.onChangeTransaction();
+        } else {
+          this.setState({
+            displayAlert: true,
+            alertMessage: "Minimum of transaction is 1 $"
+        });
+        }
+    }
+    onCloseAlert() {
+        this.setState({
+            displayAlert: false
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Container>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>
+                                Transfers {this.state.amountTransfert} $ to{" "}
+                                {this.props.userSelect}{" "}
+                            </Form.Label>
+                            <Form.Control
+                                type="number"
+                                max={findBalance()}
+                                min="0"
+                                name="amountTransfert"
+                                value={this.state.amountTransfert}
+                                onChange={this.handleChange}
+                            />
+                        </Form.Group>
+                        <Button onClick={this.handleSubmit}>Transfers</Button>
+                    </Form>
+                </Container>
+                <AlertDismissibleExample
+                    displayAlert={this.state.displayAlert}
+                    onCloseAlert={this.onCloseAlert.bind(this)}
+                    alertMessage={this.state.alertMessage}
+                />
+            </div>
+        );
+    }
+}
+function AlertDismissibleExample(props) {
+    if (props.displayAlert) {
+        return (
+            <Alert variant="danger" onClick={props.onCloseAlert} dismissible>
+                {props.alertMessage}
+            </Alert>
+        );
+    }
+    return <div></div>;
 }
 export default Transaction;
