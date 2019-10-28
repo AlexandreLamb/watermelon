e050d331ee746bd76c89bf7c925696bc4c9724a8
Siemon <alexandre.lambert@edu.ece.fr>
2019-10-28 20:20:22 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from "react";
 import "./App.css";
 import Routes from "./Components/Routes";
 
-// email prof technoweb :
-// kevin.frydman@esquad.co
-// deadline 28 octobre 23:59:59
-// Object.assaign({}, object)
 function App() {
   return (
     <div className="App">
